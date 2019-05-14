e6bb3a74891e6d505158bb200bee60afbb45c51f
sinmetal <metal.tie@gmail.com>
2019-05-14 14:36:51 +0900

    add hosting sample

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 go mod vendor
-GO111MODULE=off gcloud functions deploy SampleHandler --project metal-tile-dev2 --runtime go111 --trigger-http --region asia-northeast1
+GO111MODULE=off gcloud functions deploy HelloFunction --project sinmetal-lab --runtime go111 --trigger-http --region us-central1
 rm -r vendor
